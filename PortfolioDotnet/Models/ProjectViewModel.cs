cba1058c1d5794d714daf0afdf865cc787a43fff
Rafael Estrada <ntaticat@gmail.com>
2023-03-05 23:24:32 -0600

    initial commit

---
+++ PortfolioDotnet/Models/ProjectViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortfolioDotnet.Models
+{
+    public class ProjectViewModel
+    {
+        public int ProjectId { get; set; }
+        public string? ProjectName { get; set; }
+        public string? ProjectDescription { get; set; }
+    }
+}
